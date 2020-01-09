bbbd36e5dd505a48354ac285238de1a5ad18eb6f
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2020-01-09 21:19:34 +0100

    n/a

---
+++ failed-to-open-session-for-the-virtual-machine/commands.ps1
@@ -0,0 +1,11 @@
+disable-windowsOptionalFeature -online -featureName Microsoft-Hyper-V-All
+
+#
+#  Also disables
+#     - Microsoft-Hyper-V
+#     - Microsoft-Hyper-V-Tools-All
+#     - Microsoft-Hyper-V-Management-PowerShell
+#     - Microsoft-Hyper-V-Hypervisor
+#     - Microsoft-Hyper-V-Services
+#     - Microsoft-Hyper-V-Management-Clients
+#
