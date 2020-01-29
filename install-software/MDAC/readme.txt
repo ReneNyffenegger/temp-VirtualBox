473f110f35063aebceebb5ca65e774ca5f86992e
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2020-01-29 16:12:19 +0100

    n/a

---
+++ install-software/MDAC/readme.txt
@@ -0,0 +1,3 @@
+http://www.8bit.rs/blog/microsoft-net/running-64-bit-ace-oledb-driver-with-32-bit-office
+
+https://stackoverflow.com/questions/37710776/how-to-install-microsoft-ace-oledb-32-bit-with-office-64-bit
---
+++ install-software/MDAC/steps.ps1
@@ -0,0 +1,22 @@
+#
+#  See https://renenyffenegger.ch/notes/Companies-Products/Oracle/VM-VirtualBox/command-line/PowerShell/setting-up-environment {
+#
+$vmNameClone='MDAC'
+$instDir="c:\users\$username\Software-to-be-installed"
+
+VBoxManage clonevm $vmName --name=$vmNameClone --register
+
+VBoxManage startvm $vmNameClone
+
+VBoxManage guestcontrol $vmNameClone run --username $userName --password $password --exe reg.exe DUMMY query HKEY_LOCAL_MACHINE\Software\Microsoft\DataAccess
+VBoxManage guestcontrol $vmNameClone run --username $userName --password $password --exe reg.exe DUMMY query HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Office
+
+
+VBoxManage guestcontrol $vmNameClone mkdir    --username $userName --password $password $instDir
+
+VBoxManage guestcontrol $vmNameClone copyto   --username $userName --password $password --target-directory $instDir "$(get-location)\find-ole-db-provider.ps1"
+
+VBoxManage guestcontrol $vmNameClone run C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe --username $userName --password $password -- -command set-executionPolicy remoteSigned -scope currentUser
+VBoxManage guestcontrol $vmNameClone run C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe --username $userName --password $password -- -command $instDir\find-ole-db-provider.ps1
+
+# }
